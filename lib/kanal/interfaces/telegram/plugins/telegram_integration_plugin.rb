551a8a4cf85f3fc410dfdf5279a4b5e7f12eae96
hulimax <maxt@pie.studio>
2023-01-26 14:02:32 +0400

    wip: send image and audio to bot and from bot

--- lib/kanal/interfaces/telegram/plugins/telegram_integration_plugin.rb
+++ lib/kanal/interfaces/telegram/plugins/telegram_integration_plugin.rb
@@ -17,8 +17,14 @@ module Kanal
 
           def register_parameters(core)
             core.register_input_parameter :tg_message, readonly: true
+            core.register_input_parameter :callback
+            core.register_input_parameter :image
+            core.register_input_parameter :audio
+            core.register_input_parameter :file_link
             core.register_output_parameter :tg_text
             core.register_output_parameter :tg_reply_markup
+            core.register_output_parameter :tg_image_path
+            core.register_output_parameter :tg_audio_path
           end
 
           def register_hooks(core)
@@ -27,7 +33,14 @@ module Kanal
             end
 
             core.hooks.attach :input_before_router do |input|
-              input.body = input.tg_message.text
+              if input.tg_message.instance_of?(::Telegram::Bot::Types::CallbackQuery)
+                input.body = input.tg_message.data
+                input.callback = true
+              else
+                input.body = input.tg_message.text
+                input.image = input.tg_message.photo
+                input.audio = input.tg_message.audio
+              end
             end
 
             core.hooks.attach :output_before_returned do |input, output|
--- lib/kanal/interfaces/telegram/telegram_interface.rb
+++ lib/kanal/interfaces/telegram/telegram_interface.rb
@@ -23,17 +23,73 @@ module Kanal
         def start
           ::Telegram::Bot::Client.run(@bot_token) do |bot|
             bot.listen do |message|
-              input = @core.create_input
+              puts "message class: #{message.class}"
 
-              input.tg_message = message
+              puts "message: #{message}"
 
-              output = router.create_output_for_input input
+              if message.instance_of?(::Telegram::Bot::Types::CallbackQuery)
+                input = @core.create_input
 
-              bot.api.send_message(
-                chat_id: message.chat.id,
-                text: output.tg_text,
-                reply_markup: output.tg_reply_markup
-              )
+                input.tg_message = message
+
+                output = router.create_output_for_input input
+
+                bot.api.send_message(
+                  chat_id: message.from.id,
+                  text: output.tg_text
+                )
+
+                # if message.data == 'touch'
+                #   bot.api.send_message(chat_id: message.from.id, text: "Don't touch me!")
+                # end
+              else
+                input = @core.create_input
+
+                if message.text.nil?
+                  message.text = "EMPTY TEXT"
+                end
+
+                input.tg_message = message
+
+                if message.photo.count > 0
+                  puts message.photo[0].file_id
+                  file = bot.api.get_file(file_id: message.photo[2].file_id)
+                  file_path = file.dig('result', 'file_path')
+                  photo_url = "https://api.telegram.org/file/bot#{@bot_token}/#{file_path}"
+                  puts photo_url
+                  input.file_link = photo_url
+                end
+
+                if message.audio.instance_of?(::Telegram::Bot::Types::Audio)
+                  file = bot.api.get_file(file_id: message.audio.file_id)
+                  file_path = file.dig('result', 'file_path')
+                  audio_url = "https://api.telegram.org/file/bot#{@bot_token}/#{file_path}"
+                  puts audio_url
+                  input.file_link = audio_url
+                end
+
+                output = router.create_output_for_input input
+
+                bot.api.send_message(
+                  chat_id: message.chat.id,
+                  text: output.tg_text,
+                  reply_markup: output.tg_reply_markup
+                )
+
+                unless output.tg_image_path.nil? or output.tg_image_path.empty?
+                  bot.api.send_photo(
+                    chat_id: message.chat.id,
+                    photo: Faraday::UploadIO.new(output.tg_image_path, "image/jpeg")
+                  )
+                end
+
+                unless output.tg_audio_path.nil? or output.tg_audio_path.empty?
+                  bot.api.send_audio(
+                    chat_id: message.chat.id,
+                    audio: Faraday::UploadIO.new(output.tg_audio_path, "audio/mpeg3")
+                  )
+                end
+              end              
             end
           end
         end
