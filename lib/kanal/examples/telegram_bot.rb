92f834266a3a6e352dbc000eaf93e748fe754dfc
hulimax <maxt@pie.studio>
2023-03-30 17:34:22 +0400

    fix: wrong namespace for example

--- lib/kanal/examples/telegram_bot.rb
+++ lib/kanal/examples/telegram_bot.rb
@@ -4,36 +4,34 @@ require "kanal/core/core"
 require "kanal/interfaces/telegram/telegram_interface"
 
 module Kanal
-  module Interfaces
-    module Examples
-      module TelegramBot
-        core = Kanal::Core::Core.new
+  module Examples
+    module TelegramBot
+      core = Kanal::Core::Core.new
 
-        bot = Kanal::Interfaces::Telegram::TelegramInterface.new core, "YOUR_TOKEN"
+      bot = Kanal::Interfaces::Telegram::TelegramInterface.new core, "YOUR_TOKEN"
 
-        core.add_condition_pack :tg_text do
-          add_condition :is do
-            with_argument
-            met? do |input, _core, argument|
-              input.tg_text == argument
-            end
+      core.add_condition_pack :tg_text do
+        add_condition :is do
+          with_argument
+          met? do |input, _core, argument|
+            input.tg_text == argument
           end
         end
-
-        core.router.configure do
-          on :tg_text, is: "foo" do
-            respond do
-              tg_text "bar"
-            end
-          end
-        end
-
-        core.router.default_response do
-          tg_text "Default response"
-        end
-
-        bot.start
       end
+
+      core.router.configure do
+        on :tg_text, is: "foo" do
+          respond do
+            tg_text "bar"
+          end
+        end
+      end
+
+      core.router.default_response do
+        tg_text "Default response"
+      end
+
+      bot.start
     end
   end
 end
