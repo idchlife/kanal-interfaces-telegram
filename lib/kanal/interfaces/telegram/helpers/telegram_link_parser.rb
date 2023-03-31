f1408fc064e326306a7f6095e44689d42630ef4a
notetell <123448630+notetell@users.noreply.github.com>
2023-03-31 17:02:06 +0400

    fix: telegram link parser (#11)
    * fix: telegram link parser
    
    * fix: tests for tg interface
    
    ---------
    
    Co-authored-by: hulimax <maxt@pie.studio>

--- lib/kanal/interfaces/telegram/helpers/telegram_link_parser.rb
+++ lib/kanal/interfaces/telegram/helpers/telegram_link_parser.rb
@@ -5,10 +5,10 @@ module Kanal
     module Telegram
       module Helpers
         class TelegramLinkParser
-          def get_file_link(file_id)
+          def get_file_link(file_id, bot, bot_token)
             file = bot.api.get_file(file_id: file_id)
             file_path = file.dig('result', 'file_path')
-            "https://api.telegram.org/file/bot#{@bot_token}/#{file_path}"
+            "https://api.telegram.org/file/bot#{bot_token}/#{file_path}"
           end
         end
       end
